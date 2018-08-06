28757d70de2f6c1c5df8a0ff4788738aca58ef40
Nasiba Carrasco <nasivuela@gmail.com>
2018-08-06 23:50:51 +0200

    Remove curry

--- src/components/List1.js
+++ src/components/List1.js
@@ -7,78 +7,76 @@ const isLastItemInArr = (ind, arr) => ind === arr.length - 1;
 const hasMaxLen = (arr, max) => max === arr.length;
 
 class List1 extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputList: ["HTML"]
-    };
-
-    this.addItem = this.addItem.bind(this);
-    this.removeItem = this.removeItem.bind(this);
-    this.updateItem = this.updateItem.bind(this);
-  }
-
-  addItem(e) {
-    this.setState(prevState => {
-      return {
-        inputList: [...prevState.inputList, "HTML"]
-      };
-    });
-  }
-
-  removeItem(e) {
-    const indToRemove = parseInt(e.target.value);
-    this.setState(prevState => {
-      return {
-        inputList: prevState.inputList.filter(
-          (item, ind) => ind !== indToRemove
-        )
-      };
-    });
-  }
-
-  updateItem(indToUpdate) {
-    return event => {
-      const { value } = event.target;
-      this.setState(prevState => {
-        return {
-          inputList: prevState.inputList.map((item, ind) => {
-            return ind === indToUpdate ? value : item;
-          })
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputList: ["HTML"]
         };
-      });
-    };
-  }
 
-  render() {
-    const { inputList } = this.state;
-    return (
-      <div>
-        {inputList.map((item, ind, listArr) => {
-          if (
-            onlyOneItemInArr(listArr) ||
-            (isLastItemInArr(ind, listArr) && !hasMaxLen(listArr, maxLen))
-          ) {
-            return (
-              <div key={ind}>
-                <input value={item} onChange={this.updateItem(ind)} />
-                <button onClick={this.addItem}>+</button>
-              </div>
-            );
-          } else {
-            return (
-              <div key={ind}>
-                <input value={item} onChange={this.updateItem(ind)} />
-                <button onClick={this.removeItem} value={ind}>
-                  -
-                </button>
-              </div>
-            );
-          }
-        })}
-      </div>
-    );
-  }
+        this.addItem = this.addItem.bind(this);
+        this.removeItem = this.removeItem.bind(this);
+        this.updateItem = this.updateItem.bind(this);
+    }
+
+    addItem(e) {
+        this.setState(prevState => {
+            return {
+                inputList: [...prevState.inputList, "HTML"]
+            };
+        });
+    }
+
+    removeItem(e) {
+        const indToRemove = parseInt(e.target.dataset.ind);
+        this.setState(prevState => {
+            return {
+                inputList: prevState.inputList.filter(
+                    (item, ind) => ind !== indToRemove
+                )
+            };
+        });
+    }
+
+    updateItem(event) {
+        const { dataset, value } = event.target;
+        const indToUpdate = parseInt(dataset.ind);
+        this.setState(prevState => {
+            return {
+                inputList: prevState.inputList.map((item, ind) => {
+                    return ind === indToUpdate ? value : item;
+                })
+            };
+        });
+    }
+
+    render() {
+        const { inputList } = this.state;
+
+        return (
+            <div>
+                {inputList.map((item, ind, listArr) => {
+                    const isAddButton = onlyOneItemInArr(listArr) ||
+                    (isLastItemInArr(ind, listArr) && !hasMaxLen(listArr, maxLen));
+
+                    return (
+                        <div key={ind}>
+                            <input 
+                                value={item} 
+                                data-ind={ind} 
+                                onChange={this.updateItem}
+                            />
+                            <button
+                                onClick={isAddButton ? this.addItem : this.removeItem}
+                                data-ind={ind}
+                            >
+                                {isAddButton ? '+' : '-'}
+                            </button>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
 }
 
 export default List1;
